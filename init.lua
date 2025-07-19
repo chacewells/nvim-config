a8c060259337326d6da3e553eb11ffc697fd5ae2
awells10 <awells10@chewy.com>
2025-07-18 17:19:43 -0700

    update treesitter; add terminal mode keymappings

--- init.lua
+++ init.lua
@@ -77,3 +77,10 @@ vim.keymap.set(
   { desc = "Telescope Projects" }
 )
 
+-- terminal mappings
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-w>h]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-w>j]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-w>k]], { noremap = true, silent = true })
+vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-w>l]], { noremap = true, silent = true })
+
