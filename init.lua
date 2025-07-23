b492362d7376df5103af383dfb1279aac0e3b8ba
awells10 <awells10@chewy.com>
2025-07-22 23:35:44 -0700

    widen nvim-tree

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ require("nvim-tree").setup({
         sorter = "case_sensitive",
     },
     view = {
-        width = 30,
+        width = 40,
     },
     renderer = {
         group_empty = true,
